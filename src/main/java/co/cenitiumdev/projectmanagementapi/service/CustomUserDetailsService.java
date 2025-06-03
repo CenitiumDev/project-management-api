db835ff3240ac1279a0f4281fb28b5442cf41954
Cenitium Dev <cenitiumdev@gmail.com>
2025-06-03 14:01:14 -0500

    feat: Implement JWT authentication filter and custom user details service

---
+++ src/main/java/co/cenitiumdev/projectmanagementapi/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package co.cenitiumdev.projectmanagementapi.service;
+
+
+import co.cenitiumdev.projectmanagementapi.model.User;
+import co.cenitiumdev.projectmanagementapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>() // Lista de autoridades/roles (vacía por ahora)
+        );
+    }
+}
