f281c0acb59a8de92c18a386571f9079e009d658
Cenitium Dev <cenitiumdev@gmail.com>
2025-06-03 11:52:24 -0500

    Initial project setup with Spring Boot, H2, and Gradle

---
+++ src/main/java/co/cenitiumdev/projectmanagementapi/ProjectManagementApiApplication.java
@@ -0,0 +1,13 @@
+package co.cenitiumdev.projectmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApiApplication.class, args);
+	}
+
+}
